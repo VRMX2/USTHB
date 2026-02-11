b4019e4d92ac4b74a0c3814b083c9611ae141329
VRMX2 <lehcengrissi@gmail.com>
2026-02-11 09:37:26 -0700

    new commit

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -86,6 +86,9 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         borderBottomColor: Colors.light.border,
         zIndex: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     headerTitle: {
         fontSize: 22,
